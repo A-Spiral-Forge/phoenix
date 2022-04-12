b8774e2cb4b7a1a89c4b2167f9664add15bcc9ea
ABHAY0PARIHAR <64008676+ABHAY0PARIHAR@users.noreply.github.com>
2022-04-12 17:31:36 +0530

    Completed home and contact page

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,5 +4,6 @@ const mainController = require('./../controllers/mainController');
 const router = express.Router();
 
 router.get('/', mainController.getHomePage);
+router.get('/contact', mainController.getContactPage);
 
 module.exports = router;

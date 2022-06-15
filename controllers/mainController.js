3560ef66b183d23c63dafc60dc08549fbf51c055
ABHAY PARIHAR <64008676+A-Spiral-Forge@users.noreply.github.com>
2022-06-15 16:07:05 +0530

    Project page and responsive website (#3)
    Co-authored-by: A-Spiral-Forge <64008676+ABHAY0PARIHAR@users.noreply.github.com>

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,15 +5,51 @@ const projectMiniData = JSON.parse(
 	fs.readFileSync(path.join(__dirname, './../data/projects.json'), 'utf-8')
 );
 
+const designsData = JSON.parse(
+	fs.readFileSync(
+		path.join(__dirname, './../data/gallery-images.json'),
+		'utf-8'
+	)
+);
+
+const aboutData = JSON.parse(
+	fs.readFileSync(path.join(__dirname, './../data/about-me.json'), 'utf-8')
+);
+
 exports.getHomePage = (req, res, next) => {
-	// console.log(projectMiniData.projectsmini);
+	// console.log(projectMiniData.projectsmini.slice(0, 4));
 
 	res.status(200).render('home', {
 		title: 'Home',
-		projects: projectMiniData.projectsmini,
+		projects: projectMiniData.projectsmini.slice(0, 4),
 	});
 };
 
 exports.getContactPage = (req, res, next) => {
-	res.status(200).render('contact', { title: 'Contact' });
+	res.status(200).render('Contact', { title: 'Contact' });
+};
+
+exports.getGalleryPage = (req, res, next) => {
+	res.status(200).render('Gallery', {
+		title: 'Gallery',
+		designs: designsData,
+	});
+};
+
+exports.getAboutPage = (req, res, next) => {
+	res.status(200).render('about', { title: 'About', details: aboutData });
+};
+
+exports.getProjectsPage = (req, res, next) => {
+	if (!req.query.tag || req.query.tag === '') req.query.tag = 'All';
+
+	const projects = projectMiniData.projectsmini.filter((project) =>
+		project.keywords.includes(req.query.tag)
+	);
+
+	res.status(200).render('projects', {
+		title: 'Projects',
+		projects: projects,
+		tags: projectMiniData.tags,
+	});
 };
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/', mainController.getHomePage);
 router.get('/contact', mainController.getContactPage);
+router.get('/designs', mainController.getGalleryPage);
+router.get('/about', mainController.getAboutPage);
+router.get('/projects', mainController.getProjectsPage);
 
 module.exports = router;

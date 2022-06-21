7fe935c8889d4f31bd03f1a966fbf9e000523445
A-Spiral-Forge <64008676+ABHAY0PARIHAR@users.noreply.github.com>
2022-06-21 19:34:53 +0530

    About completed and minor fixes

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,25 +1,6 @@
-const tagForm = document.querySelector('.form--search');
-const selectPicker = document.querySelector('#tags');
-const dropdown = document.querySelector('.tags--select--custom');
-const dropdownList = document.querySelector('.tags--select--custom ul');
-
-selectPicker.value = document.querySelector('p.tags').dataset.value;
-
-tagForm.addEventListener('click', (e) => {
-	const el = e.target.closest('.tags--select--custom');
-	if (!el) return;
-	el.classList.toggle('collapse');
-});
-
-document.addEventListener('click', (e) => {
-	const el = e.target.closest('.tags--select--custom');
-	if (el) return;
-	dropdown.classList.remove('collapse');
-});
-
-dropdownList.addEventListener('click', (e) => {
-	const el = e.target.closest('.tags');
-	if (!el) return;
+exports.changeTag = (el) => {
+	const selectPicker = document.querySelector('#tags');
+	selectPicker.value = document.querySelector('p.tags').dataset.value;
 	document.querySelector('p.tags').textContent = el.textContent;
 	selectPicker.value = el.textContent;
-});
+};
